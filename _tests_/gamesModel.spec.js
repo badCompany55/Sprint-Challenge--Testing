73f113f368e990bb99ac3ced084ac98de76fd4f7
badCompany55 <zachirvin45@gmail.com>
2019-04-20 11:59:48 -0500

    GET GAMES FUNCTION TEST add get games function test/ gamesModel

--- _tests_/gamesModel.spec.js
+++ _tests_/gamesModel.spec.js
@@ -7,6 +7,19 @@ const testGame = {
   releaseYear: 1980
 };
 
+const testGames = [
+  {
+    title: "Pacman",
+    genre: "Aracde",
+    releaseYear: 1980
+  },
+  {
+    title: "Mario",
+    genre: "Aracde",
+    releaseYear: null
+  }
+];
+
 describe("games model", () => {
   beforeEach(() => {
     return db("games").truncate();
@@ -22,6 +35,15 @@ describe("games model", () => {
       expect(insertObject).toHaveProperty("title", "Pacman");
       expect(insertObject).toHaveProperty("genre", "Aracde");
       expect(insertObject).toHaveProperty("releaseYear", 1980);
+    });
+  });
+
+  describe("get games method", () => {
+    it("should retrieve a list of games", async () => {
+      await db("games").insert(testGames);
+      const allGames = await games.getGames();
+
+      expect(allGames.length).toBe(2);
     });
   });
 });
