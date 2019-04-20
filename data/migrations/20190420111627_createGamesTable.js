bc00b7ed6b1bfd1c53a41ff43724c85dfbb03f11
badCompany55 <zachirvin45@gmail.com>
2019-04-20 11:23:29 -0500

    GAMES TABLE create games table schema create table for dev create table for testing

--- data/migrations/20190420111627_createGamesTable.js
+++ data/migrations/20190420111627_createGamesTable.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+	return knex.schema.createTable("games", tb => {
+		tb.increments()
+		tb.string("title", 128).notNullable().unique()
+		tb.string("genre", 128).notNullable()
+		tb.integer("releaseYear", 4)
+	}) 
 };
 
 exports.down = function(knex, Promise) {
-  
+	return knex.schema.dropTableIfExists("games")
 };
